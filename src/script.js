089fafe7cfe133ae560f90c7c29fc30965b83106
sheillanjoroge <sheillan.njoroge@gmail.com>
2021-02-12 13:10:53 +0300

    add the geolocation functionality for handling errors in js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,19 @@
+// method to get user's location
+var x = document.getElementById("demo");
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+}
+// Method to display the results using static image
+
 function showPosition(position) {
       var latlon = position.coords.latitude + "," + position.coords.longitude;
     
@@ -9,3 +25,20 @@ function showPosition(position) {
        document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
     
      }
+    //  Function for handling errors and and rejection
+    function showError(error) {
+        switch(error.code) {
+          case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+          case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+          case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+          case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+        }
+      }
