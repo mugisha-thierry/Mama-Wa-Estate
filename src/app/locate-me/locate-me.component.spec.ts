29f3180de657392bdf87d7f559aa6e63360887cc
sheillanjoroge <sheillan.njoroge@gmail.com>
2021-02-12 12:26:39 +0300

    making correction

--- src/app/locate-me/locate-me.component.spec.ts
+++ src/app/locate-me/locate-me.component.spec.ts
@@ -24,34 +24,4 @@ describe('LocateMeComponent', () => {
   });
 });
 
-//this function is to get the current position
-locateMe() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position) => {
-      this.showPosition(position);
-    });
-  } else {
-    alert("Geolocation is not supported by this browser.");
-  }
-}
-
-//this is for plotting marker
-showPosition(position) {
-  this.currentLat = position.coords.latitude;
-  this.currentLong = position.coords.longitude;
-
-  let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-  this.map.panTo(location);
-
-  if (!this.marker) {
-    this.marker = new google.maps.Marker({
-      position: location,
-      map: this.map,
-      title: 'Got you!'
-    });
-  }
-  else {
-    this.marker.setPosition(location);
-  }
-}
 
--- src/app/locate-me/locate-me.component.ts
+++ src/app/locate-me/locate-me.component.ts
@@ -7,6 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocateMeComponent implements OnInit {
 
+  
+locateMe() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.showPosition(position);
+    });
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
+
+//this is for plotting marker
+showPosition(position) {
+  this.currentLat = position.coords.latitude;
+  this.currentLong = position.coords.longitude;
+
+  let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  this.map.panTo(location);
+
+  if (!this.marker) {
+    this.marker = new google.maps.Marker({
+      position: location,
+      map: this.map,
+      title: 'Got you!'
+    });
+  }
+  else {
+    this.marker.setPosition(location);
+  }
+}
+
+
   constructor() { }
 
   ngOnInit(): void {
