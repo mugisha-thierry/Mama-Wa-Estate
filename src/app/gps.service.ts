be29b61e740ed64109928b934c9e94336597e0f4
sheillanjoroge <sheillan.njoroge@gmail.com>
2021-02-13 10:50:24 +0300

    add the geolocation service function

--- src/app/gps.service.ts
+++ src/app/gps.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GpsService {
+  http: HttpClient
+  private user
+  isUserChecked: boolean
+  private searchedLong: string
+  private searchedLat: string
+  private searchLongChange: Subject<string> = new Subject<string>()
+  private searchLatChange: Subject<string> = new Subject<string>()
+  private LatChange: Subject<string> = new Subject<string>()
+  private Lat: string;
+
 
   constructor() { }
 }
