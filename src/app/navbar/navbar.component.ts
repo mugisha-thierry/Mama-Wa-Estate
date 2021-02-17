05258400f1a929562789a8ea20eeba19dac0327e
sheillanjoroge <sheillan.njoroge@gmail.com>
2021-02-17 14:01:50 +0300

    location functionality enabled, the app can now access the lat and long of the user

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,16 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const L: any;
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
   locateMe() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
@@ -23,4 +20,51 @@ export class NavbarComponent implements OnInit {
       alert("Geolocation is not supported by this browser.");
     }
   }
+  watchPosition(){
+    let deslat = 0;
+    let deslon = 0;
+    let id = navigator.geolocation.watchPosition(
+      (position) => {
+      console.log(
+        'lat: ${position.coords.latitude}, lon: ${position.coords.longitude}'
+      );
+    },(err) => {
+      console.log(err);
+    },{
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    })
+    
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+    if (!navigator.geolocation) {
+      console.log('location is not supported');
+
+      navigator.geolocation.getCurrentPosition( position => {
+        const coords = position.coords;
+        console.log(
+          'lat: ${position.coords.latitude}, lon: ${position.coords.longitude}'
+          );
+        let mymap = L.map('map').setView([coords.latitude, coords.longitude], 13);
+        
+        L.tileLayer
+        ('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2hlaWxsYW5qb3JvZ2UiLCJhIjoiY2tsOTJld2EzMGxjcTJvcHN0ODlzYnY3ZCJ9.0RxxkogFC6yzMzsWe6bVuQ', {
+          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+          id: 'mapbox/streets-v11',
+          tileSize: 512,
+          zoomOffset: -1,
+          accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
+      
+      });
+    }
+  }
+  
+
+
 }
