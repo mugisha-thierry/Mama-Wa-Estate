f5480f567298183fe262e9a27a53eaf5da8ef447
sheillanjoroge <sheillan.njoroge@gmail.com>
2021-02-17 04:33:24 +0300

    add host listener to execute actions

--- src/app/locate.directive.ts
+++ src/app/locate.directive.ts
@@ -8,5 +8,15 @@ export class LocateDirective {
   constructor(private elem: ElementRef) {
     this.elem.nativeElement.style.textDecoration='line-through';
    }
+  @HostListener("click") onClicks(){
+    this.textDeco("line-through")
+  }
+
+  @HostListener("dblclick") onDoubleClicks(){
+    this.textDeco("None")
+  }
+  private textDeco(action:string) {
+    this.elem.nativeElement.style.textDecoration=action;
+  }
 
 }
