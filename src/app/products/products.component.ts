0f75eb8a65812072a1e0de3637a13d47e387feef
Mugisha Thierry <thierrymugis@gmail.com>
2021-02-21 20:12:02 +0200

    filtered product based on id

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ParamMap, ActivatedRoute } from '@angular/router';
+import { ProductsService } from '../products.service';
+import { map } from "rxjs/operators";
 
 @Component({
   selector: 'app-products',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  product;
+  constructor(private _productService: ProductsService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-  }
+    this.route.paramMap.pipe(
+      map((param: ParamMap) => {
+        // @ts-ignore
+        return param.params.id;
+      })
+    ).subscribe(prodId => {
+      this.id = prodId;
+      this._productService.getproduct(this.id).subscribe(prod => {
+        this.product = prod;
+      });
+    });
 
+  }
 }
+
+  // ngOnInit(): void {
+  //   this.route.paramMap.pipe(
+  //     map((param: ParamMap) => {
+  //       // @ts-ignore
+  //       return param.params.id;
+  //     })
+  // //   this._productService.getproduct(this.id)
+  // //   .subscribe(
+  // //     res => this.product = res,
+  // //     err => console.log(err)
+  // //   )
+  // // }
