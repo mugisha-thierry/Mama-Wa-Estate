334e6192b8d82fe0b86018a45bf8646ad1aed849
Mugisha Thierry <thierrymugis@gmail.com>
2021-02-21 20:14:25 +0200

    sonsumed store api

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  
+  products = []
+  constructor(private _productService: ProductsService, private router:Router) { }
 
   ngOnInit(): void {
+    // this._productService.getAllproducts()
+    // .subscribe(
+    //   res => this.products = res,
+    //   err => console.log(err)
+    // )
   }
 
+
+  // selectProduct(id: Number) {
+  //   this.router.navigate(['/product', id]).then();
+  // }
+
+  
 }
+
