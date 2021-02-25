0a8490bf5ea4b927bddbf7a0c8f6c4a8192f8900
sheillanjoroge <sheillan.njoroge@gmail.com>
2021-02-25 10:53:12 +0300

    change the category appearance

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -3,10 +3,7 @@ import { AddToCartService } from '../add-to-cart.service';
 import { Router } from '@angular/router';
 import { DOCUMENT } from '@angular/common';
 import { FlashMessagesService } from 'ngx-flash-messages';
-<<<<<<< HEAD
-=======
 // import { totalmem } from 'os';
->>>>>>> 3e535169411e6959bab9756d82e443c97dd60c0b
 
 @Component({
   selector: 'app-cart',
@@ -14,10 +11,7 @@ import { FlashMessagesService } from 'ngx-flash-messages';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-<<<<<<< HEAD
-=======
   [x: string]: any;
->>>>>>> 3e535169411e6959bab9756d82e443c97dd60c0b
   body=[]
   products =[]
   constructor(@Inject(DOCUMENT) private _document: Document,private flashMessagesService: FlashMessagesService,private _router: Router,private _cartProductsService:AddToCartService) { }
@@ -38,11 +32,7 @@ export class CartComponent implements OnInit {
       res=>{
       console.log('ok')
       this.refreshPage()
-<<<<<<< HEAD
-      this.flashMessagesService.show('Product successful', {
-=======
       this.flashMessagesService.show('Product successful deleted', {
->>>>>>> 3e535169411e6959bab9756d82e443c97dd60c0b
         classes: ['alert', 'alert-success'], // You can pass as many classes as you need
         timeout: 5000, // Default is 3000
       });
@@ -55,14 +45,10 @@ export class CartComponent implements OnInit {
     this._document.defaultView.location.reload();
   }
   calculate(x:number,y:number){
-<<<<<<< HEAD
-    return x*y
-=======
     let total:Number;
     total = x*y;
     return total
   
->>>>>>> 3e535169411e6959bab9756d82e443c97dd60c0b
   }
   update(id:number){
     this._cartProductsService.updateQty(id,this.body)
